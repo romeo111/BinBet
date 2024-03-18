878be63ccab6f6463d02c511cbc80a87cf73b8d3
romeo111 <805@ukr.net>
2024-03-18 15:46:40 +0200

    v

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './binbet';
 import reportWebVitals from './reportWebVitals';
+//
+import { HashRouter as Router } from 'react-router-dom';
+//
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

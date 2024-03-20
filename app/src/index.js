181a71d91bfda1e018f9be6b94e27e6bb6cbd24f
romeo111 <805@ukr.net>
2024-03-20 16:32:52 +0200

    df

--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Corrected import statement
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './binbet';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter basename="/Binbet">
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
-
+// Use createRoot instead of ReactDOM.render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
+
+// Remove ReactDOM.render call
+
+// You can keep other code as it is
 
 reportWebVitals();

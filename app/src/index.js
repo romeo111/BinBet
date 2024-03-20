5b7a7925a080a0999c23ef9df4588a7f04f0bcfa
romeo111 <805@ukr.net>
2024-03-20 15:27:21 +0200

    vv

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Corrected import statement
 import './index.css';
 import App from './binbet';
 import reportWebVitals from './reportWebVitals';
-//
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
@@ -12,7 +11,6 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
 );
-//
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -21,7 +19,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
